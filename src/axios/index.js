6a1e86cba50a9d11d87aa90d166cd4fb7537ecfc
Timeless Ventures <timeless@192.168.1.6>
2023-04-07 04:49:54 +0530

    Add existing project files to Git

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 // Creating axios instance
 
 const instance = axios.create({
-    baseURL: ' http://localhost:8005',
+    //baseURL: ' http://localhost:8005',
+
+    baseURL: "https://nomatic-server.onrender.com"
 
 })
 
